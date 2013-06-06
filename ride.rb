44cebffa692446b368f9db35ae86f11b984ccb3e
Muhammad Usman <muhammad@squaretrade.com>
2013-06-05 17:25:40 -0700

    rankings

--- ride.rb
+++ ride.rb
@@ -4,7 +4,7 @@ require 'open-uri'
 
 def getStats(user, early)
   page = Nokogiri::HTML(open("http://strava.com/athletes/#{user}"))
-  result = [ page.css("h1#athlete-name").text, page.css("ul.inline-stats li strong")[3].text.to_f.round(2) - early]
+  result = [ page.css("h1#athlete-name").text[0..-4], page.css("ul.inline-stats li strong")[3].text.to_f.round(2) - early]
 end
 
 users = {"usmanity" => 211, "hcabalic" => 697, "1320215" => 655, "1689644" => 53, "2285604" => 0, "1902953" => 71}
@@ -18,6 +18,8 @@ get "/" do
   users.each { |user, early|
     new = getStats(user, early)
     @riders.merge!({new[0] => new[1]})
+    @rides = @riders.sort_by { |k| k[1] }
+    @rides.reverse!
   }
-  erb :index, :locals => {:riders =>  @riders }
+  erb :index, :locals => {:riders =>  @rides }
 end
